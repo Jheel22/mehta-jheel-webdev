ecd995daa7865df409c41c35985cf308fb53c03d
jheel93 <mehta.jh@husky.neu.edu>
2017-06-13 00:56:35 -0400

    Assignment 5 done

--- assignment/app.js
+++ assignment/app.js
@@ -1,5 +1,16 @@
-var mongoose = require('mongoose');
-mongoose.createConnection('mongodb://localhost/test');
+/*var mongoose = require('mongoose');
+ mongoose.createConnection('mongodb://localhost/test');*/
+var connectionString = 'mongodb://localhost/test'; // for local
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds137291.mlab.com:37291/heroku_x8rc58vn'; // user yours
+}
+
+
+var mongoose = require("mongoose");
+mongoose.createConnection(connectionString);
 mongoose.Promise = require('q').Promise;
 
 require('./services/user.service.server');
